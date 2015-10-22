23f6398ecd71f888138b949b591813c70a6f697b
Ian Palmgren <ianmpal@gmail.com>
2015-10-21 22:17:26 -0700

    Final Working Copy

--- SimpleCalcRedux/SimpleCalcRedux/ViewController.swift
+++ SimpleCalcRedux/SimpleCalcRedux/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var isTyping = true
+    var isTyping = false
     var firstNumber = 0.0
     var secondNumber = 0.0
     var operation = ""
@@ -29,23 +29,20 @@ class ViewController: UIViewController {
     
     @IBAction func clearButton(sender: UIButton) {
         isTyping = false
-        calculatorDisplay!.text = ""
+        calculatorDisplay!.text = "0.0"
         resultsArray = []
     }
     
     @IBAction func buttonOne(sender: UIButton) {
+        NSLog("Button pushed")
         let number = sender.currentTitle
+        NSLog(number!)
         
         if isTyping {
-            let newNum = calculatorDisplay!.text! + number!
-            calculatorDisplay!.text = newNum
-            firstNumber = Double(newNum)!
-            NSLog(resultsArray.description)
+            calculatorDisplay!.text = calculatorDisplay!.text! + number!
         } else {
             calculatorDisplay.text = number
-            resultsArray.append(Double(number!)!)
-            NSLog("else statement" + resultsArray.description)
-
+            isTyping = true
         }
     }
     
@@ -53,28 +50,7 @@ class ViewController: UIViewController {
     @IBAction func calculationTapped(sender: AnyObject) {
         operation = (sender.currentTitle!)!
         isTyping = false
-    }
-    
-    @IBAction func operandButton(sender: UIButton) {
-        NSLog("Operand button pressed")
-        isTyping = false
-        operation = (sender.currentTitle!)
-        var result = 0.0
-        
-        
-        switch operation {
-        case "count":
-            resultsArray.append(firstNumber)
-            NSLog("Hit count")
-        case "fact":
-            result = fact(firstNumber)
-            resultsArray = []
-            calculatorDisplay.text = "\(result)"
-        default:
-            calculatorDisplay.text = "\(result)"
-            
-        }
-        
+        firstNumber = Double(calculatorDisplay.text!)!
     }
     
     @IBAction func equalsTapped(sender: AnyObject) {
@@ -87,44 +63,67 @@ class ViewController: UIViewController {
             isTyping = false
             secondNumber = Double(calculatorDisplay.text!)!
             result = firstNumber + secondNumber
-            resultsArray = []
         case "-":
             isTyping = false
             secondNumber = Double(calculatorDisplay.text!)!
             result = firstNumber - secondNumber
-            resultsArray = []
         case "*":
             isTyping = false
             secondNumber = Double(calculatorDisplay.text!)!
             result = firstNumber * secondNumber
-            resultsArray = []
         case "/":
             isTyping = false
             secondNumber = Double(calculatorDisplay.text!)!
             result = firstNumber / secondNumber
-            resultsArray = []
         case "%":
             isTyping = false
             secondNumber = Double(calculatorDisplay.text!)!
             result = firstNumber % secondNumber
-            resultsArray = []
         case "count":
+            resultsArray.append(Double(calculatorDisplay.text!)!)
+            for num in resultsArray {
+                NSLog(String(num))
+            }
             result = Double(resultsArray.count)
-            NSLog(String(resultsArray.description))
-            resultsArray = []
         case "avg":
+            resultsArray.append(Double(calculatorDisplay.text!)!)
             var sum = 0.0
             for num in resultsArray {
                 sum += num
             }
             result = sum / Double(resultsArray.count)
-            resultsArray = []
         default:
             calculatorDisplay.text = "\(result)"
 
         }
         calculatorDisplay.text = "\(result)"
         
+    }
+    
+    
+    
+    
+    @IBAction func operandButton(sender: UIButton) {
+        NSLog("Operand button pressed")
+        isTyping = false
+        operation = (sender.currentTitle!)
+        var result = 0.0
+        firstNumber = Double(calculatorDisplay.text!)!
+        
+       
+        switch operation {
+        case "count":
+            resultsArray.append(firstNumber)
+            NSLog("Hit count")
+        case "fact":
+            result = fact(firstNumber)
+            calculatorDisplay.text = "\(result)"
+        case "avg":
+            resultsArray.append(firstNumber)
+        default:
+            calculatorDisplay.text = "\(result)"
+            
+        }
     }
     
     
@@ -141,115 +140,3 @@ class ViewController: UIViewController {
 
 
 }
-
-/*
-
-import Foundation
-
-func input() -> String {
-    let keyboard = NSFileHandle.fileHandleWithStandardInput()
-    let inputData = keyboard.availableData
-    let result = NSString(data: inputData, encoding:NSUTF8StringEncoding) as! String
-    return result.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
-}
-
-func convert(incoming:String) -> Double {
-    return NSNumberFormatter().numberFromString(incoming)!.doubleValue
-}
-
-func count(input: [String]) -> Int {
-    return (input.count)
-}
-
-func avg(input: [String]) -> Double {
-    var total: Double = 0
-    var count: Double = 0
-    for number in input {
-        total += Double(convert(number))
-        count++
-    }
-    return total / count
-}
-
-func fact(input: [String]) -> Double {
-    var inputNum = convert(input[0])
-    var result = 1.0
-    while (inputNum > 0) {
-        result = result * inputNum
-        inputNum--
-    }
-    return result
-}
-
-
-var keepCalculating = true
-while keepCalculating {
-    
-    let input1 = input()
-    var textArray = input1.characters.split{$0 == " "}.map(String.init)
-    let lastIndex: String = textArray[textArray.count-1].lowercaseString
-    
-    if lastIndex.rangeOfString("count") != nil || lastIndex.rangeOfString("fact") != nil || lastIndex.rangeOfString("avg") != nil {
-        
-        textArray.removeAtIndex(textArray.count-1)
-        if input1.lowercaseString.rangeOfString("count") != nil {
-            print("There are \(count(textArray)) numbers total.")
-        } else if input1.lowercaseString.rangeOfString("avg") != nil {
-            print("The average is: \(avg(textArray))")
-        } else if input1.lowercaseString.rangeOfString("fact") != nil {
-            if textArray.count == 1 {
-                print("The factorial of \(textArray[0]) = \(fact(textArray))")
-            } else {
-                print("A factorial operation can only take in one number at a time. Please try again.")
-            }
-        }
-        
-    } else {
-        let num1 = convert(input1)
-        
-        print("Enter an operation")
-        var operation = input()
-        var validOperation = true
-        while validOperation {
-            if operation.rangeOfString("+") == nil && operation.rangeOfString("-") == nil && operation.rangeOfString("/") == nil && operation.rangeOfString("*") == nil && operation.rangeOfString("%") == nil {
-                print("Please enter a valid operation (+ - / * %).")
-                operation = input()
-            } else {
-                validOperation = false
-            }
-        }
-        
-        print("Enter a number")
-        let num2 = convert(input())
-        
-        var result = 0.0
-        var resultDouble: Double = 0
-        
-        if operation == "+" {
-            result = num1 + num2
-        } else if operation == "-" {
-            result = num1 - num2
-        } else if operation == "/" {
-            resultDouble = Double(num1)/Double(num2)
-        } else if operation == "*" {
-            result = num1*num2
-        } else if operation == "%" {
-            result = num1%num2
-        }
-        
-        if operation == "/" {
-            print("The result of \(num1) \(operation) \(num2) =  \(resultDouble)")
-        } else {
-            print("The result of \(num1) \(operation) \(num2) =  \(result)")
-        }
-        
-    }
-    
-    print("\n\n" + "Would you like to perform another calculation? Please enter \"y\" or \"n\".")
-    if input().lowercaseString.rangeOfString("n") != nil {
-        keepCalculating = false
-    }
-    
-}
-
-*/
